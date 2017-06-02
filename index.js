c06403a08050e76f5094e7d0f0da4cbd834ab4e4
moshe <github email address>
2017-06-02 17:00:29 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+    return true
+};
+
+function sayHey(){
+    return "Hey!"
+};
+
+function sayHeyFriend(name){
+    return `Hey, ${name}!`
+};
+
+function favoriteIceCream(flavor){
+    return `I love ${flavor}`
+};
+
+function shouting(sentance){
+   return sentance.toUpperCase()
+};
+
+function roundDown(number){
+  return Math.floor(number)
+}
